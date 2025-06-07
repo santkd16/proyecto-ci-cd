887ee85181a7b4d09ab206e7709aa3ecb9d50d25
santkd16 <santiagocabezas16@gmail.com>
2025-06-07 01:14:26 -0500

    sa

--- servicio2/app.py
+++ servicio2/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hola desde Servicio 2'
+    return 'Hola desde Servicio 5'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
