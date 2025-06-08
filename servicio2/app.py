85b5f3ca9764b613e12ca44c40021ed873a55fd6
santkd16 <santiagocabezas16@gmail.com>
2025-06-08 00:18:32 -0500

    sa

--- servicio2/app.py
+++ servicio2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hola desde Servicio 2'
+    return 'Hola desde Servicio 2 prue'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
